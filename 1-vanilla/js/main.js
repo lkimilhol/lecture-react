427a814ef32d4f2f34b3753ee83e9862a6e78114
lkimilhol <lkimilhol@gmail.com>
2023-03-06 23:18:04 +0900

    [순수JS 1] 검색폼 1

--- 1-vanilla/js/main.js
+++ 1-vanilla/js/main.js
@@ -1,19 +1,20 @@
 import Controller from "./Controller.js";
 import Store from "./store.js";
 import storage from "./storage.js";
+import SearchFormView from "./views/SearchFormView.js";
 
-const tag = '[main]'
+const tag = "[main]";
 document.addEventListener("DOMContentLoaded", main);
 
 // MVC 패턴을 사용한다
 function main() {
-  console.log(tag, 'main')
+  console.log(tag, "main");
 
   // 모델을 생성한다
   const store = new Store(storage);
 
   const views = {
-    // TODO
+    searchFormView: new SearchFormView()
   };
 
   new Controller(store, views);
